45706ee7fed286018b1f6157a9bc061bdb019053
Gage Woodall <gagewoodall0831@gmail.com>
2023-01-03 16:33:54 -0600

    Add files via upload

---
+++ src/pages/Account.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/84526d58-475e-4e6f-9c81-d2d78ddce803/0593e91e-9312-4435-9ec3-abd5611d0dac/US-en-20221228-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
+
+export default Account;
